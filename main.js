4fb1c82af1b35e823bdd2be9c9b3c15a63f1d933
Nicholas <shianicho@gmail.com>
2023-09-03 18:17:05 +0700

    tambah dropdown

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+// dropdown jumlah
 const boxJumlah = document.querySelector('#box-jumlah');
 const barJumlah = document.querySelector('#bar-jumlah');
 const pJumlah = document.querySelector('#p-jumlah');
@@ -10,10 +12,31 @@ boxJumlah.addEventListener('click', () => {
 
   if (barJumlah.classList.contains('active')) {
     pJumlah.classList.add('hidden');
-    chevronDown.style = `transform : rotate(180deg);
-    transition: .2s ease-in-out`
+    chevronDown.classList.add('active-rotate');
   } else if (barJumlah.classList.contains('hidden')) {
     pJumlah.classList.remove('hidden');
+    chevronDown.classList.remove('active-rotate');
   }
 });
+
+// dropdown ikhtisar material
+const ikhtisar = document.querySelector('#ikhtisar');
+const material = document.querySelector('#material');
+
+ikhtisar.addEventListener('click', () => {
+  const ikhtisarParagraf = document.querySelector('#ikhtisar-paragraf');
+  const chevronDown = document.querySelector('.chevron-down-ikhtisar');
+  
+  ikhtisarParagraf.classList.toggle('hidden');
+  chevronDown.classList.toggle('active-rotate');
+  
+});
+
+material.addEventListener('click', () => {
+  const materialParagraf = document.querySelector('#material-paragraf');
+  const chevronDown = document.querySelector('.chevron-down-material');
+
+  materialParagraf.classList.toggle('hidden');
+  chevronDown.classList.toggle('active-rotate');
+}); 
 
